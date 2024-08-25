7393b985f787fc9f869e75d5a05ffbf3a6967745
Sanna Olsson <saol1901@student.miun.se>
2024-08-25 21:22:56 +0200

    updat.

--- server.js
+++ server.js
@@ -3,10 +3,12 @@ const bodyParser = require('body-parser');
 const autRoutes = require("./routes/autRoutes");
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
+const cors = require('cors');
 
 
 
 const app = express();
+app.use(cors());
 const port = process.env.port || 3000;
 app.use(bodyParser.json());
 
@@ -22,15 +24,20 @@ function authenticateToken(req, res, next){
     const authHeader = req.headers['authorization'];
     const token = authHeader && authHeader.split(' ')[1];
     
-    if(token == null) res.status(404).json({message: 'not authorized for this route - token missing'});
+    if(token == null) {
+        return res.status(404).json({message: 'not authorized for this route - token missing'});
+    }
 
-    jwt.verify(token, process.env.JWT_SECRET_KEY, (err, username)=>{
-        if(err)return res.status(403).json({message: 'not correct jwt'});
+    jwt.verify(token, process.env.JWT_SECRET_KEY, (err, username) => {
+        if(err) {
+            return res.status(403).json({message: 'not correct jwt'});
+        }
 
         req.username = username;
         next();
     });
 }
+
 
 //start applikation
 app.listen(port, ()=>
